084c2f07a3dc94c98ff37620ba0260d3942614f9
Nils Tonagel <ntonagel@me.com>
2023-07-08 13:14:45 +0200

    Fix tsconfig.json bundle issue

--- MauiBundler/MauiBundler/Components/MauiBundlerView.xaml.cs
+++ MauiBundler/MauiBundler/Components/MauiBundlerView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Microsoft.AspNetCore.Components.WebView;
 
 namespace MauiBundler.Components;
 
@@ -8,5 +9,17 @@ public partial class MauiBundlerView : ContentPage
 	public MauiBundlerView()
 	{
 		InitializeComponent();
+		mauiWebView.BlazorWebViewInitialized += BlazorWebViewInitialized;
 	}
+
+    private void BlazorWebViewInitialized(object? sender, BlazorWebViewInitializedEventArgs e) 
+    {
+#if IOS || MACCATALYST
+        if ((DeviceInfo.Current.Platform == DevicePlatform.MacCatalyst && DeviceInfo.Current.Version >= new Version(13, 3)) ||
+            (DeviceInfo.Current.Platform == DevicePlatform.iOS && DeviceInfo.Current.Version >= new Version(16, 4)))
+        {
+            e.WebView.SetValueForKey(Foundation.NSObject.FromObject(true), new Foundation.NSString("inspectable"));
+        }
+#endif
+    }
 }
