6cb5d4296370e7853a140e5b9108d385ac448b47
Nils Tonagel <ntonagel@me.com>
2023-07-08 13:19:10 +0200

    Fix warnings

--- MauiBundler/Plugins/Plugin.Geolocation/Services/GeolocationService.macios.cs
+++ MauiBundler/Plugins/Plugin.Geolocation/Services/GeolocationService.macios.cs
@@ -28,7 +28,7 @@ public class GeoLocationService : IGeoLocationService
             var status = await Permissions.RequestAsync<Permissions.LocationWhenInUse>();
             if (status != PermissionStatus.Granted)
             {
-                Task.Run(async () => await PublishStatusChangedEvent("Permission for location is not granted, we can't get location updates"));
+                Task.Run(async () => await PublishStatusChangedEvent("Permission for location is not granted, we can't get location updates")).GetAwaiter().GetResult();
                 return;
             }
             iosLocationManager.RequestAlwaysAuthorization();
@@ -37,10 +37,10 @@ public class GeoLocationService : IGeoLocationService
         });
     }
 
-     private void LocationsUpdated(object sender, CLLocationsUpdatedEventArgs e)
+     private void LocationsUpdated(object? sender, CLLocationsUpdatedEventArgs? e)
         {
-            var locations = e.Locations;
-            var loc = new Models.Location(locations[^1].Coordinate.Latitude, locations[^1].Coordinate.Longitude, (float)locations[^1].Course, locations[^1].Altitude, (float)locations[^1].CourseAccuracy);
+            var locations = e!.Locations;
+            var loc = new Models.Location(locations[^1].Coordinate.Latitude, locations[^1].Coordinate.Longitude, (float)locations[^1].Course, locations[^1].Altitude, (float)locations[^1].CourseAccuracy!);
             Task.Run(async () => await jsRuntime.InprocessJSRuntime!.InvokeVoidAsync($"MauiBundler.Plugins.{nameof(GeoLocation)}.locationChanged", loc));
         }
 
